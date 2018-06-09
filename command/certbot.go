4abd76d9a53b4dc12800e85b0684320b8d0a5b80
David N Salazar <dsalazar@iomediums.com>
2018-06-09 00:20:28 -0400

    Added some helpers to help format the flags that will be passed to the certbot cli.

--- command/certbot.go
+++ command/certbot.go
@@ -1,15 +1,21 @@
 package command
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // This just acts as a proxy to the certbot project.
 // The resulting certificates will then be handed off to
 // logic that will interact with AWS iam certificate manager.
+// TODO: Write test
 // TODO: Detect certbot installed
 // TODO: Setup proxy logic
-// TODO: Upload resulting cert to s3 for backup (Optional).
-// TODO: Nice to have would be a DNS host name validator.
-type Certbot struct {
+// TODO: Upload resulting cert to s3 for backup (Optional)
+// TODO: Nice to have would be a DNS host name validator
+type CertbotCommand struct {
+	CertbotFlags []certbotFlag
+
 	Meta
 }
 
@@ -22,16 +28,11 @@ type certbotFlag struct {
 
 type domains []string
 
+// var certbotCommand = "certonly"
 var domainsFlag domains
-var email string
-var certbotCommand = "certonly"
-var certbotFlags = []certbotFlag{
-	{"-n", "Run non-interactively"},
-	{"--agree-tos", "Agree to the ACME server's Subscriber Agreement"},
-	{"--dns-route53", "Email address for important account information"},
-}
+var emailFlag string
 
-func (c *Certbot) Help() string {
+func (c *CertbotCommand) Help() string {
 	return "implement me"
 }
 
@@ -44,16 +45,52 @@ func (d *domains) Set(v string) error {
 	return nil
 }
 
-func (c *Certbot) Run(args []string) int {
-	f := c.Meta.flagSet("Certbot")
+func (c *CertbotCommand) Run(args []string) int {
+	f := c.Meta.flagSet("CertbotCommand")
 	f.Var(&domainsFlag, "d", "Comma-separated list of domains to obtain a certificate for")
-	f.StringVar(&email, "email", "", "Email address for important account notifications")
+	f.StringVar(&emailFlag, "email", "", "Email address for important account notifications")
 	if err := f.Parse(args); err != nil {
 		return 1
 	}
+
+	// These will be the default flags that will be proxied to the certbot cli.
+	c.setCertbotFlag("-n", "Run non-interactively")
+	c.setCertbotFlag("--agree-tos", "Agree to the ACME server's Subscriber Agreement")
+	c.setCertbotFlag("--dns-route53", "Use route53 for the challenge")
+	c.setCertbotFlag("-email", emailFlag)
+	c.setCertbotFlag("-d", domainsFlag)
+
+	fmt.Printf("%v", c.CertbotFlags)
 	return 0
 }
 
-func (c *Certbot) Synopsis() string {
+func (c *CertbotCommand) Synopsis() string {
 	return `This tool just acts as a proxy to the certbot project. The resulting artifacts (certificates) will be used to update AWS Certificate Manager.`
+}
+
+func (c *CertbotCommand) CommandString(f []certbotFlag) string {
+	return ""
+}
+
+func (c *CertbotCommand) setCertbotFlag(k string, v interface{}) {
+	var cf certbotFlag
+	switch v.(type) {
+	case string:
+		vt := v.(string)
+		if vt == "" {
+			return
+		}
+		cf = certbotFlag{fmt.Sprintf("%s %s", k, vt), ""}
+	case domains:
+		vt := v.(domains)
+		if len(vt) == 0 {
+			return
+		}
+		var c []string
+		for _, f := range vt {
+			c = append(c, fmt.Sprintf("%s %s", k, f))
+		}
+		cf = certbotFlag{strings.Join(c, " "), ""}
+	}
+	c.CertbotFlags = append(c.CertbotFlags, cf)
 }
--- commands.go
+++ commands.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/mitchellh/cli"
 	"github.com/dsalazar32/go-certbotify-aws/command"
+	"github.com/mitchellh/cli"
 )
 
 var Commands map[string]cli.CommandFactory
@@ -14,7 +14,7 @@ func initCommands() map[string]cli.CommandFactory {
 
 	Commands = map[string]cli.CommandFactory{
 		"certbot": func() (cli.Command, error) {
-			return &command.Certbot{
+			return &command.CertbotCommand{
 				Meta: meta,
 			}, nil
 		},
