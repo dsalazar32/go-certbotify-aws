a12a6756c61a20c7ee186b5bfc4e9658b68c188d
David N Salazar <dsalazar@iomediums.com>
2018-06-17 20:40:21 -0400

    Updates exec prefix to support OS

--- command/utils/commander.go
+++ command/utils/commander.go
@@ -7,7 +7,7 @@ import (
 
 func Commander(cwd string, eVars ...string) func(c string, output bool) ([]byte, error) {
 	cmd := func(c string, output bool) ([]byte, error) {
-		command := exec.Command("bash", "-c", c)
+		command := exec.Command("sh", "-c", c)
 		command.Env = append(os.Environ(), eVars...)
 		command.Dir = cwd
 		if output == true {
